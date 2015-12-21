fd938885c7924943101a3f36bd8b335bf333a08a
Gavin Tam <gavintam168@gmail.com>
2015-12-21 23:27:39 +1100

    Added all pages and updated UI

---
+++ footerFormat.js
@@ -0,0 +1,43 @@
+window.onload = formatPage;
+window.onresize = formatPage;
+
+function formatPage() {
+	var footer = document.getElementById("footer");
+	var footerStyle = window.getComputedStyle(footer);
+	var height = "-" + footerStyle.height.toString();
+	footer.style.bottom = height;
+	console.log(footerStyle.height.toString());	
+}
+
+(function ($) {
+
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+
+$.fn.waitUntilExists    = function (handler, shouldRunHandlerOnce, isChild) {
+    var found       = 'found';
+    var $this       = $(this.selector);
+    var $elements   = $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+
+    if (!isChild)
+    {
+        (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+            window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+        ;
+    }
+    else if (shouldRunHandlerOnce && $elements.length)
+    {
+        window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+    }
+
+    return $this;
+}
+
+}(jQuery));
+
+$("#footer .row").waitUntilExists(formatPage);
